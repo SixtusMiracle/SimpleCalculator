512eb19b2c39e9cff097eac13c3984f05344718f
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-01-05 02:57:13 +0100

    added ui components

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,53 @@ using System.Windows.Shapes;
 
 namespace SimpleCalculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    double currentValue = 0;
+    enum Operation { Add, Subtract, Multiply, Divide, Equals, Start };
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
+      txtOut.Text = currentValue.ToString();
     }
+
+    private void BtnEntry_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void Calculate(Operation op)
+    {
+    }
+
+    // 4 event handlers for operations:
+    private void BtnAdd_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void BtnSubtract_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void BtnMultiply_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void BtnDivide_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    //Clear the current results
+    private void BtnClear_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    //Handle the Equals button
+    private void BtnEquals_Click(object sender, RoutedEventArgs e)
+    {
+    }
+  }
 }
